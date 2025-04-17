3ce4c5815d909ffaf7cfbfe1ac986ee8420e75d9
inlinkcraft <inlinkcraft@gmail.com>
2025-04-17 19:36:06 -0400

    Fin de plusieurs test ?

---
+++ mesurements.py
@@ -0,0 +1,47 @@
+
+# En mètre
+SIZE = {
+    
+    "HEAD":{
+        "NECK":0.42,
+        "JAW":0.50,
+        "EARS":0.58,
+        "FOREHEAD":0.59
+    },
+    
+    "RIGHT_ARM":{
+        "RIGHT_WRIST":0.185,
+        "RIGHT_FORARM":0.29,
+        "RIGHT_ELBOW":0.295,
+        "RIGHT_BICEP":0.365,
+    },
+    
+    "LEFT_ARM":{
+        "WRIST":0.18,
+        "FORARM":0.29,
+        "ELBOW":0.29,
+        "BICEP":0.385
+    },
+    
+    "TORSO":{
+        "PECK":1.09,
+        "BELLY":1.055,
+        "WAIST":1.01,
+        "HIPS":1.09
+    },
+    
+    "RIGHT_LEG":{
+        "THIGH":0.64,
+        "KNEE":0.40,
+        "CALF":0.38,
+        "ANKLE":0.24
+    },
+    
+    "LEFT_LEG":{
+        "THIGH":0.64,
+        "KNEE":0.40,
+        "CALF":0.365,
+        "ANKLE":0.24
+    }
+    
+}
